806eebf54d6cab0469de5e4b8c941e341866beb2
shubhamshahane1234 <82734945+shubhamshahane1234@users.noreply.github.com>
2021-04-18 08:20:49 +0530

    Update Contact.js

--- src/mycomponents/Contact.js
+++ src/mycomponents/Contact.js
@@ -7,22 +7,22 @@ export default function Contact() {
 
 
 <form className = "p-5 ">
-  <div className="mb-3 w-50">
+  <div className="mb-3" style = {{maxWidth : "500px"}}>
     <label for="exampleInputEmail1" className="form-label">First Name :</label>
     <input type="email" placeholder="name here" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
     
   </div>
-  <div className="mb-3 w-50">
+  <div className="mb-3" style = {{maxWidth : "500px"}}>
     <label for="exampleInputPassword1" className="form-label">Last Name :</label>
     <input type="text" placeholder="last here" className="form-control" id="exampleInputPassword1"/>
   </div>
 
-  <div className="mb-3 w-50">
+  <div className="mb-3 " style = {{maxWidth : "500px"}}>
     <label for="exampleInputPassword1"  className="form-label">Phone :</label>
     <input type="text"  placeholder="phone number here" className="form-control" id="exampleInputPassword1"/>
   </div>
 
-  <div className="mb-3 w-50">
+  <div className="mb-3 " style = {{maxWidth : "500px"}}>
     <label for="exampleInputPassword1"  className="form-label">Comment :</label>
     <textarea className="form-control" placeholder="comment here" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
